ae59f66d59e59e73007095e1797ba267452ae5f3
Camilo Zambrano <camilozambranovotto@hotmail.com>
2018-05-21 14:17:20 -0500

    Code review instagram.js

--- imports/api/instagram.js
+++ imports/api/instagram.js
@@ -3,6 +3,7 @@ import {HTTP} from "meteor/http";
 
 Meteor.methods({
     "instagram.get"(hashtag) {
+        // Camilo Zambrano: You are not checking the hastag here or on App.js, this has potential security problems for your app
         try {
             const res = HTTP.get(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`);
             return res.data.graphql.hashtag.edge_hashtag_to_media.edges;
